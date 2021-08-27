a68957b3504df7b1e314692ed9c6c3c166902e58
b.vukasovic <b.vukasovic@sportradar.com>
2021-08-27 21:33:50 +0200

    animation and not working

--- src/aurelia-demo.ts
+++ src/aurelia-demo.ts
@@ -2,8 +2,15 @@ import {SomeDemo} from "./components/some-demo/some-demo";
 import {SomeDemo2} from "./components/some-demo2/some-demo2";
 
 export class AureliaDemo{
-  public components = [
-      SomeDemo,
-      SomeDemo2
+  public components : [any] = [
+      SomeDemo
   ];
+
+  render = () => {
+    this.components.push(SomeDemo2);
+  }
+
+  rimuv = () => {
+    this.components.pop();
+  }
 }
--- src/components/some-demo2/some-demo2.ts
+++ src/components/some-demo2/some-demo2.ts
@@ -3,7 +3,26 @@ import {SomeApi} from "../../services/some-api";
 export class SomeDemo2{
   private tekst: any = {};
 
-  constructor(private someApi: SomeApi) {
+  constructor(private someApi: SomeApi, private readonly host: Element) {
+
+  }
+
+  attaching = () => {
+    const animation = this.host.animate(
+        [{ opacity: 1 }, { opacity: 0 }],
+        { duration: 10000 },
+    );
+    console.log(animation);
+    return animation.finished;
+  }
+
+  detaching = () => {
+    console.log("detaching");
+    const animation = this.host.animate(
+        [{ transform: 'translateX(-300px)' }, { transform: 'translateX(0)' }],
+        { duration: 50000 },
+    );
+    return animation.finished;
   }
 
   async binding(): Promise<void> {
